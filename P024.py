f1b7d997f805fe7278a40a22f9acd3c6dbd93e37
flexo333 <willb5@gmail.com>
2014-07-20 22:45:34 +1000

    Sunday Night
    Minor Update to P024

--- P024.py
+++ P024.py
@@ -7,13 +7,14 @@ def lexi(number_l):
 	else:
 		for x in number_list:
 			for y in lexi(number_list[:-len(number_list)+1]):
-				yield str(x) + str(y)   
-
+				yield str(x) + str(y)
+				
 
 
 def problem():
-	print map(int,lexi(list([3,5,8,9])))
-
+	# print map(int,lexi(list([3,5,8,9])))
+	for item in lexi([3,5,8,9]):
+		print item
 
 if __name__ == "__main__":
 	problem()
