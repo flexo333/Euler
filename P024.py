8882c5a4edd2f34583e2d5c55cced5178afb2502
William Bright <William.Bright@anz.com>
2014-07-24 21:15:41 +1000

    Completed P024  - Includes both calculated and brute force versions

--- P024.py
+++ P024.py
@@ -7,29 +7,35 @@ def lexi(number_l):
 			for y in lexi([i for i in number_list if i != x]):
 				yield str(x) + str(y)
 				
-
 def fact(n):
 	prod = 1
-	for x in xrange(11-n,n+1):
+	for x in xrange(1,n+1):
 		prod *= x
 	return prod
 
-
 def problem():
-	# print map(int,lexi(list([3,5,8,9])))
-	# for item in lexi([3,5,8,9]):
-	# 	print item
 	# for i in xrange(p):
-	my_list = [fact(i) for i in xrange(10,0,-1)]
-	test = 10
+	my_list = [fact(i) for i in xrange(9,0,-1)]
+	test = 10**6
+
 	my_counts = []
 	for item in my_list:
 		x,y = divmod(test,item)
-		my_counts.append(x)
+		if y == 0:
+			my_counts.append(max([x-1,0]))
+		else:
+			my_counts.append(x)
 		test = y
-	print ''.join(map(str, my_counts))
-
-	print test
+	num_list = list(xrange(0,10))
+	
+	answer=''
+	for item in my_counts:
+		if item ==0:
+			answer += str(num_list.pop(-1))
+		else:
+			answer += str(num_list.pop(item))
+	answer += str(num_list.pop(0))
+	return answer
 
 def getPermutations(string):
     if len(string) == 1:
@@ -39,9 +45,16 @@ def getPermutations(string):
             for perm in getPermutations(string[:i] + string[i+1:]):
                 yield string[i] + perm
 
-if __name__ == "__main__":
-	# for i in getPermutations('012'):
-	# 	print i
-	# print map(str,lexi(list([0,1,2,3,4,5,6,7,8,9])))
+def brute_force():
+	counter = 0
+	for i in (lexi(list([0,1,2,3,4,5,6,7,8,9]))):
+		counter += 1
+		if counter == 1000000:
+			print i
+			break
 
-	print [sum for i in (lexi(list([0,1,2,3,4,5,6,7,8,9])))]
+
+if __name__ == "__main__":
+
+	# brute_force()
+	print problem()
