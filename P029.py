908a6c1ef720a9c24b93c936ca0a293e1855d799
unknown <brightw@L3C970EC1F1BD.global.anz.com>
2014-07-31 23:13:16 +1000

    29 Done.

---
+++ P029.py
@@ -0,0 +1,12 @@
+
+
+def problem():
+	whole_list = []
+	for a in range(2, 101):
+		for b in range(2, 101):
+			whole_list.append(a**b)
+	short_list = set(whole_list)
+	return len(short_list)
+
+if __name__ == "__main__":
+	print problem()
