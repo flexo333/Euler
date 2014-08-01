17bd36f9a96e1426ca32df273b25c83781530a5e
William Bright <William.Bright@anz.com>
2014-08-01 11:38:53 +1000

    Tuning for P027

--- P027.py
+++ P027.py
@@ -1,24 +1,41 @@
-from eulertools import isprime
+import eulertools as et
 
 def my_function(n,a,b):
 	result = n**2 + a * n + b
 	return result
 
-
 def problem():
-	maxn = 38
-
+	maxn = 0
 	for a in xrange(-1000, 1000):
 			for b in xrange(-1000, 1000):
 				n = 0
 
-				while isprime(my_function(n, a, b)):
+				while et.isprime(my_function(n, a, b)):
 					n += 1
 				if n > maxn:
 					maxn = n
 					answer = [a,b,n-1]
+
 	return answer[0] * answer[1]
 
 
+def problem_wsimon():
+	maxn = 0
+	P =set(et.primeseive(10000))
+	for a in xrange(-1000, 1000):
+			for b in xrange(-1000, 1000):
+				n = 0
+
+				while n**2 + a * n + b in P:
+					n += 1
+				if n > maxn:
+					maxn = n
+					answer = a * b
+
+	return answer
+
 if __name__ == "__main__":
 	print problem()
+
+
+
