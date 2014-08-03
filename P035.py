001147954bc5a50e98ab0fea697356836e61ee28
unknown <brightw@L3C970EC1F1BD.global.anz.com>
2014-08-03 23:18:34 +1000

    Started on P035

---
+++ P035.py
@@ -0,0 +1,16 @@
+import eulertools
+
+
+def problem():
+	primelist = eulertools.primeseive(1000000)
+
+	for i in range(2,1000000):
+		for d in str(i):
+			pass
+
+
+
+
+
+if __name__ == "__main__":
+	print problem()	
